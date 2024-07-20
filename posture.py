3b5db7f8d2f6f92a63025ef934ceca1b52b8e3db
Sahil Dhavale <sahil.234226102@vcet.edu.in>
2024-07-20 20:50:08 +0530

    v1.01

--- posture.py
+++ posture.py
@@ -29,35 +29,34 @@ def calculate_angle(a,b,c):
 
 
 cap = cv2.VideoCapture(0)
-## Setup mediapipe instance
 with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
     while cap.isOpened():
         ret, frame = cap.read()
         
-        # Recolor image to RGB
+       
         image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         image.flags.writeable = False
       
-        # Make detection
+       
         results = pose.process(image)
     
-        # Recolor back to BGR
+        
         image.flags.writeable = True
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
         
-        # Extract landmarks
+       
         try:
             landmarks = results.pose_landmarks.landmark
             
-            # Get coordinates
+            
             shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
             elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
             wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
             
-            # Calculate angle
+           
             angle = calculate_angle(shoulder, elbow, wrist)
             
-            # Visualize angle
+            
             cv2.putText(image, str(angle), 
                            tuple(np.multiply(elbow, [640, 480]).astype(int)), 
                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
@@ -67,7 +66,7 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
             pass
         
         
-        # Render detections
+       
         mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
                                 mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2), 
                                 mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2) 
